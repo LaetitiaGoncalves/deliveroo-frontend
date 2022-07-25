56aea43711625bbdd0ee68d4752c4d947522ecac
Laetitia Goncalves <laetitiagoncalves@macbook-pro-de-laetitia.home>
2022-07-25 22:50:38 +0200

    css add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import logo from "./images/logo-teal.svg";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStar, faListAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+library.add(faStar, faListAlt);
 
 const App = () => {
   const [data, setData] = useState(0);
@@ -25,37 +30,76 @@ const App = () => {
     <span>En cours de chargement</span>
   ) : (
     <div className="App">
-      <img src="./images/logo-teal.svg" alt="" />
-      <h1>{data.restaurant.name}</h1>
-      <p>{data.restaurant.description}</p>
-      <div>
-        <div>
-          {data.categories.map((categorie, index) => {
-            return (
-              <div key={index}>
-                <h2>{categorie.name}</h2>
-                <div>
-                  {categorie.meals.map((title, index) => {
-                    return (
-                      <div key={index}>
-                        <h3>{title.title}</h3>
-                        <p>{title.description}</p>
-                        <p>{title.price}</p>
-                        <p>{title.popular ? <span>Populaire</span> : ""}</p>
-                        <p>
-                          {title.picture ? (
-                            <img src={title.picture} alt="" />
-                          ) : (
-                            ""
-                          )}
-                        </p>
-                      </div>
-                    );
-                  })}
+      <header>
+        <div className="top-bar">
+          <div className="top-bar-center">
+            <img src={logo} alt="logo Deliveroo" className="logo" />
+          </div>
+        </div>
+      </header>
+      <div className="restaurant-div">
+        <div className="restaurant-infos">
+          <h1>{data.restaurant.name}</h1>
+          <p>{data.restaurant.description}</p>
+        </div>
+        <div className="restaurant-info-picture">
+          <img src={data.restaurant.picture} alt="" />
+        </div>
+      </div>
+
+      <div className="main-content">
+        <div className="menu-center">
+          <div className="menu">
+            {data.categories.map((categorie, index) => {
+              return (
+                <div key={index}>
+                  <h2>{categorie.name}</h2>
+                  <div className="menu-items">
+                    {categorie.meals.map((title, index) => {
+                      return (
+                        <div key={index} className="card">
+                          <div className="card-item">
+                            <div className="card-text">
+                              <h3>{title.title}</h3>
+                              <p>{title.description}</p>
+                              <span className="price">
+                                {title.price.replace(".", ",")}€
+                              </span>
+                              <span className="popular">
+                                {title.popular ? (
+                                  <span className="star">
+                                    <FontAwesomeIcon icon="fa-solid fa-star" />
+                                    Populaire
+                                  </span>
+                                ) : (
+                                  ""
+                                )}
+                              </span>
+                            </div>
+                            <div className="card-picture">
+                              <>
+                                {title.picture ? (
+                                  <img src={title.picture} alt="" />
+                                ) : (
+                                  ""
+                                )}
+                              </>
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
+          <div className="panier">
+            <div>
+              <button>Valider mon panier</button>
+              <p>Votre panier est vide</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
